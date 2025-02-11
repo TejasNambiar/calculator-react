406d1532db271dd55ffb69b64e58f298e29797b9
Tejas Nambiar <tejasnambiar2810@outlook.com>
2025-02-11 12:19:50 +0530

    modularized calculator body

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import CalculatorBody from './CalculatorBody'
 
 function App() {
   const [input, setInput] = useState('0')
@@ -67,36 +68,7 @@ function App() {
     <div className='container'>
       <div className='calc'>
         <h1 id='input'>{input}</h1>
-        <div>
-          <button onClick={()=> handleButtonClick('C')}>C</button>
-          <button onClick={()=> handleButtonClick('<')}>&lt;</button>
-          <button onClick={()=> handleButtonClick('%')}>%</button>
-          <button onClick={()=> handleButtonClick('/')}>/</button>
-        </div>
-        <div>
-          <button onClick={()=> handleButtonClick('7')}>7</button>
-          <button onClick={()=> handleButtonClick('8')}>8</button>
-          <button onClick={()=> handleButtonClick('9')}>9</button>
-          <button onClick={()=> handleButtonClick('*')}>*</button>
-        </div>
-        <div>
-          <button onClick={()=> handleButtonClick('4')}>4</button>
-          <button onClick={()=> handleButtonClick('5')}>5</button>
-          <button onClick={()=> handleButtonClick('6')}>6</button>
-          <button onClick={()=> handleButtonClick('-')}>-</button>
-        </div>
-        <div>
-          <button onClick={()=> handleButtonClick('3')}>3</button>
-          <button onClick={()=> handleButtonClick('2')}>2</button>
-          <button onClick={()=> handleButtonClick('1')}>1</button>
-          <button onClick={()=> handleButtonClick('+')}>+</button>
-        </div>
-        <div>
-          <button onClick={()=> handleButtonClick('0')}>0</button>
-          <button onClick={()=> handleButtonClick('00')}>00</button>
-          <button onClick={()=> handleButtonClick('.')}>.</button>
-          <button onClick={()=> handleButtonClick('=')}>=</button>
-        </div>
+        <CalculatorBody handleButtonClick={handleButtonClick}/>
       </div>
     </div>
   )
---
+++ src/CalculatorBody.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const CalculatorBody = ({ handleButtonClick }) => {
+    return (
+      <>
+        <div>
+          <button onClick={() => handleButtonClick('C')}>C</button>
+          <button onClick={() => handleButtonClick('<')}>&lt;</button>
+          <button onClick={() => handleButtonClick('%')}>%</button>
+          <button onClick={() => handleButtonClick('/')}>/</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleButtonClick('*')}>*</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleButtonClick('-')}>-</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('+')}>+</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={() => handleButtonClick('00')}>00</button>
+          <button onClick={() => handleButtonClick('.')}>.</button>
+          <button onClick={() => handleButtonClick('=')}>=</button>
+        </div>
+      </>
+    );
+  };
+  
+  export default CalculatorBody;
